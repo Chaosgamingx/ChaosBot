36458d04b8e19ce31b09b9123418bb31b7c15258
Chaosgamingx <78837643+Chaosgamingx@users.noreply.github.com>
2021-02-10 11:13:49 -0600

    Add files via upload

---
+++ commands/roll.js
@@ -0,0 +1,23 @@
+const { MessageEmbed, Message } = require("discord.js");
+
+module.exports = {
+  name: "roll",
+  description: "rolls a die",
+  category: "misc",
+
+  //IntelliSense
+  /**
+   * @param {Message} message
+   * @param {Client} client
+   * @param {String[]} args
+   */
+
+  execute(message, args) {
+    const randomRoll = Math.floor(Math.random() * 6) + 1;
+    const roll = new MessageEmbed();
+    roll.setTitle("Roll!");
+    roll.setDescription(`The dice rolled ${randomRoll}`);
+    roll.setColor("RANDOM");
+    message.channel.send(roll);
+  },
+};
