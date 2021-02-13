b579662af32782cd78efca822a38bd0ddfe9a7d3
Chaosgamingx <78837643+Chaosgamingx@users.noreply.github.com>
2021-02-12 20:34:39 -0600

    Add files via upload

---
+++ commands/trumpmeme.js
@@ -0,0 +1,26 @@
+module.exports = {
+    name: 'trumpmeme',
+    description: "trumpmeme",
+    execute(client, message, args) {
+  
+      
+        message.channel.send('https://www.nydailynews.com/resizer/8yAAR3ARqSqcRotWFMeJclBIOvM=/1200x0/top/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/YHLE2P2NBRPA3NHPNXNRVZDMKI.jpg');
+  
+      }
+  
+  
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
