13583f9e3eff960f846ada54dfd4656d3105c742
Chaosgamingx <78837643+Chaosgamingx@users.noreply.github.com>
2021-02-13 22:56:59 -0600

    Add files via upload

---
+++ commands/commandlist.js
@@ -0,0 +1,38 @@
+module.exports = {
+    name: 'commandlist',
+    description: "displays the commands",
+execute(client, message, cmd, args, Discord){
+             const embed = new Discord.MessageEmbed()
+            .setTitle('List of commands')
+            .setColor('RANDOM')
+            .setDescription(`
+            **Mod commands**
+            -ban (bans members)
+            -clear (clears chat up to 14 days)
+            -kick (kicks members)
+            -mute (keeps a member from talking)
+            -unmute (unmutes the member)
+            
+            **Music commands**
+            -play (plays a song)
+            -skip (skips the song that's playing)
+            -disconnect (disconnects the bot from VC)
+            
+            **Meme commands**
+            -bidoof (pokemon)
+            -mouthwash (mouthwash)
+            -rickrolled (don't)
+            -trumpmeme (displays a trump meme)
+            
+            **Misc. commands**
+            -invite (sends a link for the bot invite)
+            -commandlist (lists the commands for the bot)
+            -reminder (set a reminder from discord)
+            -ping (shows the bots ping info)
+            -serverinfo (displays some info about the server)
+            -userinfo (displays a users information)
+            -weather (displays weather for a state or city)`); 
+
+        message.channel.send(embed)              
+    }
+    }
