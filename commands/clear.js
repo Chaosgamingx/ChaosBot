41d2481c3850b80335b875970178d76851df4614
Chaosgamingx <78837643+Chaosgamingx@users.noreply.github.com>
2021-03-01 18:48:30 -0600

    fixed commands

--- commands/clear.js
+++ commands/clear.js
@@ -1,22 +1,33 @@
 module.exports = {
     name: 'clear',
     description: "clears text!",
-    async execute(client, message, args) {
+   async execute(client, message, args) {
 
-        if (message.member.permissions.has("MANAGE_MESSAGES "))  {
-
-        if(!args[0]) return message.reply("please enter an amount to delete");
-        if(isNaN(args[0])) return message.reply("please enter a real number");
-
-        if(args[0] > 100) return message.reply("you can't delete more than 100 messages");
-        if(args[0] < 1) return message.reply("you can't do 0 or below. Minimum is 1");
-
-        await message.channel.messages.fetch({limit: args[0]}).then(messages =>{
-            message.channel.bulkDelete(messages);
-        });
-    } else {
-        message.channel.send('you are not a moderator.');
-
-            }   
+        if (!message.member.hasPermission('MANAGE_MESSAGES'))
+            return message.channel.send("You cant use this command since you're missing `manage_messages` perm");
+        if (!isNaN(message.content.split(' ')[1])) {
+            let amount = 0;
+            if (message.content.split(' ')[1] === '1' || message.content.split(' ')[1] === '0') {
+                amount = 1;
+            } else {
+                amount = message.content.split(' ')[1];
+                if (amount > 100) {
+                    amount = 100;
+                }
+            }
+            await message.channel.bulkDelete(amount, true).then((_message) => {
+                message.channel.send(`Bot cleared \`${_message.size}\` messages :broom:`).then((sent) => {
+                    setTimeout(function () {
+                        sent.delete();
+                    }, 2500);
+                });
+            });
+        } else {
+            message.channel.send('enter the amount of messages that you would like to clear').then((sent) => {
+                setTimeout(function () {
+                    sent.delete();
+                }, 2500);
+            });
+        }
     }
-    }
+}
