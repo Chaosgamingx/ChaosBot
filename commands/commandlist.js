24ac74c0fc453cc9e580f4e18a803108a14f3dba
chaosgamingx <gamingxlegend13@gmail.com>
2021-03-10 20:34:32 -0600

    bug fixcd

--- commands/commandlist.js
+++ commands/commandlist.js
@@ -7,46 +7,49 @@ execute(client, message, cmd, args, Discord){
             .setColor('RANDOM')
             .setDescription(`
             **Mod commands**
-            -ban (bans members)
-            -kick (kicks members)
-            -mute (mutes a member)
-            -unmute (unmutes a member)
-            -clear (clears text of the persons choice)
+            ban (bans members)
+            kick (kicks members)
+            mute (mutes a member)
+            unmute (unmutes a member)
+            clear (clears text of the persons choice)
             
             **Music commands**
-            -play (plays a song)
-            -skip (skips the song that's playing)
-            -disconnect (disconnects the bot from VC)
+            play (plays a song)
+            skip (skips the song that's playing)
+            disconnect (disconnects the bot from VC)
+            search (display a song on youtube)
+
             
             **Meme commands**
-            -bidoof (pokemon)
-            -mouthwash (mouthwash)
-            -rickrolled (don't)
-            -trumpmeme (displays a trump meme)
-            -meme (displays a random meme)
-            -fight (fight a user)
+            bidoof (pokemon)
+            mouthwash (mouthwash)
+            rickrolled (don't)
+            trumpmeme (displays a trump meme)
+            meme (displays a random meme)
+            fight (fight a user)
+            tableflip (shows a VERY fast table flip)
 
             **Games**
-            -rps (a rock paper scissors game)
-            -roll (rolls a die)
+            rps (a rock paper scissors game)
+            roll (rolls a die)
             
             **Anime Stuff**
-            -pat (pat a user)
-            -poke (poke a user)
+            pat (pat a user)
+            poke (poke a user)
 
             **Misc. commands**
-            -invite (sends a link for the bot invite)
-            -commandlist (lists the commands for the bot)
-            -reminder (set a reminder from discord)
-            -ping (shows the bots ping info)
-            -serverinfo (displays some info about the server)
-            -userinfo (displays a users information)
-            -avatar (displays a users avatar)
-            -ChaosBot (displays basic info about the bot)
-            -perms (displays perms for users)
-            -steam (display a steam game)
-            -wikipedia (display stuff from wikipedia)
-            -weather (displays weather for a state or city)`); 
+            invite (sends a link for the bot invite)
+            commandlist (lists the commands for the bot)
+            reminder (set a reminder from discord)
+            ping (shows the bots ping info)
+            serverinfo (displays some info about the server)
+            userinfo (displays a users information)
+            avatar (displays a users avatar)
+            ChaosBot (displays basic info about the bot)
+            perms (displays perms for users)
+            steam (display a steam game)
+            wikipedia (display stuff from wikipedia)
+            weather (displays weather for a state or city)`); 
 
         message.channel.send(embed)              
     }
--- commands/invite.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'invite',
-    description: "this is a bot invite command!",
-    execute(client, message, args) {
-        message.channel.send("Want to invite ChaosBot to your server? then here's a link for you:  https://discord.com/oauth2/authorize?client_id=530267263501074443&scope=bot&permissions=2147483647");
-    }
-}
