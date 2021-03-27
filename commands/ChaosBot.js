2b6c0144892d3319db476e2cb80afccbbec7bf08
Chaosgamingx <78837643+Chaosgamingx@users.noreply.github.com>
2021-03-26 20:36:37 -0500

    Update ChaosBot.js

--- commands/ChaosBot.js
+++ commands/ChaosBot.js
@@ -7,7 +7,7 @@ module.exports = {
             .setTitle('ChaosBot')
             .setThumbnail('https://images.discordapp.net/avatars/530267263501074443/faa6f020a403dfb3dd0b24fb10cea33c.png?size=256')
             .setColor('RANDOM')
-            .setDescription(`ChaosBot is an all-in-one bot made up by peoples ideas.
+            .setDescription(`ChaosBot is an all-in-one bot made up by peoples ideas and put into the bot by the maker: Chaosgamingx.
             if you need help with the bot you can go here: https://discord.gg/W8stFj5bXB
             You can also go here to get a look at the bots cool layout: https://github.com/Chaosgamingx/ChaosBot`);
 
