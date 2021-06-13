a3b50ab0cbe24d3f4c272c1c1b13a00b32e83290
Chaosgamingx <78837643+Chaosgamingx@users.noreply.github.com>
2021-06-12 22:23:44 -0500

    Update jazzybird.js

--- commands/images/jazzybird.js
+++ commands/images/jazzybird.js
@@ -1,9 +1,10 @@
 module.exports = {
-  name: 'jazzybird',
-  description: 'jazzybird',
-  category: 'images',
+    name: 'jazzybird',
+    aliases: [],
+    category: 'images',
+    utilisation: '{prefix}jazzybird',
 
-  execute(client, message, args) {
-message.channel.send("https://media1.tenor.com/images/560b123e645878156c3885ad13e238b9/tenor.gif?itemid=21939065")
-  }
+    execute(client, message, args) {
+        message.channel.send("https://media1.tenor.com/images/560b123e645878156c3885ad13e238b9/tenor.gif?itemid=21939065");
+    }
 }
