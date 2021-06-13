baba30502d4ab1f5c68f95b7843f9c87b1e17e37
Chaosgamingx <78837643+Chaosgamingx@users.noreply.github.com>
2021-06-12 22:20:57 -0500

    Create jazzybird.js

---
+++ commands/images/jazzybird.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'jazzybird',
+  description: 'jazzybird',
+  category: 'images',
+
+  execute(client, message, args) {
+message.channel.send("https://media1.tenor.com/images/560b123e645878156c3885ad13e238b9/tenor.gif?itemid=21939065")
+  }
+}
