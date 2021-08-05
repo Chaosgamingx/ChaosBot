ea013a75e976cfdc91eee6cea2afc711cb4edec2
Chaosgamingx <78837643+Chaosgamingx@users.noreply.github.com>
2021-08-05 02:37:59 -0500

    Add files via upload

--- commands/economy/beg.js
+++ commands/economy/beg.js
@@ -27,24 +27,64 @@ module.exports = {
             }
             else {
 
-                const response = await profileModel.findOneAndUpdate({
-                    userID: message.author.id,
-                }, {
-                    $inc: {
-                        coin: randomNumber,
-                    }
-                })
-                const embed = new Discord.MessageEmbed()
-                    .setDescription(`**${message.author.username}**, you begged and recieved **${randomNumber} Chaos coins**`)
-                    .setColor("RANDOM")
-                    .setTimestamp()
-                message.channel.send(embed)
+                let convstring = Number(randomNumber)
+
+                const failorsuccess = ['Success', 'Failed']
+                let failrand = Math.floor((Math.random() * failorsuccess.length))
+                let failss = failorsuccess[failrand]
+
+                //the beg money givers
+                const beggiver = [`Dwayne Johnson`, `Selena Gomez`, `Justin Bieber`, `Rick Astley`, `Alan Walker`, `Random Stranger`,]
+                let begresultgive = Math.floor((Math.random() * beggiver.length))
+                let beggive = beggiver[begresultgive]
+
+                // description when you use beg command
+                const begdescription = [`You little poor beggar. here take **$${convstring} Chaos credits**`, `Here take **$${convstring} Chaos credits** and invest it.`, `Aw man i only got $30 in my wallet.. I can give you **$${convstring} Chaos credits**`]
+                let begresultdesc = Math.floor((Math.random() * begdescription.length))
+                let begres = begdescription[begresultdesc]
+
+                // description when you failed at begging
+                const beggingfail = ['Get a job man', "kekw here's $200 ||sike||", 'get out of my sight', 'go ask your momma', 'ask someone else', 'go ask somewhere else', 'you failed to recieve money from someone']
+                let failbegrand = Math.floor((Math.random() * beggingfail.length))
+                let failbeg = beggingfail[failbegrand]
+
+
+                if (failss === 'Success') {
+
+                    const response = await profileModel.findOneAndUpdate({
+                        userID: message.author.id,
+                    }, {
+                        $inc: {
+                            coin: convstring,
+                        }
+                    })
+                    const begembed = new Discord.MessageEmbed()
+                        .setTitle(`**${beggive}**`)
+                        .setDescription(`**${begres}**`)
+                        .setColor("#fcba03")
+                        .setTimestamp(Date.now())
+
+                    //send the begembed in channel
+                    message.channel.send(begembed)
+
+                } else if (failss === 'Failed') {
+
+                    const failembed = new Discord.MessageEmbed()
+                        .setTitle(`**${beggive}**`)
+                        .setDescription(`**${failbeg}`)
+                        .setColor("#fcba03")
+                        .setTimestamp(Date.now())
+
+                    //send the failembed in channel
+                    message.channel.send(failembed)
+                }
+                talkedRecently.add(message.author.id);
+                setTimeout(() => {
+                    // Removes the user from the set after a minute
+                    talkedRecently.delete(message.author.id);
+                }, 3600000);
+
             }
-            talkedRecently.add(message.author.id);
-            setTimeout(() => {
-                // Removes the user from the set after a minute
-                talkedRecently.delete(message.author.id);
-            }, 3600000 );
         }
     },
 }
