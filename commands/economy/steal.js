669b01e243e092eb474719f157bccc1fdbffbac8
Chaosgamingx <78837643+Chaosgamingx@users.noreply.github.com>
2021-08-10 12:21:07 -0500

    Add files via upload
    New Economy commands

--- commands/economy/steal.js
+++ commands/economy/steal.js
@@ -3,13 +3,13 @@ const Discord = require('discord.js')
 const talkedRecently = new Set();
 
 module.exports = {
-    name: 'beg',
+    name: 'steal',
     aliases: [],
     category: 'economy',
     utilisation: '{prefix}help <command name>',
 
     async execute(client, message, args) {
-        const randomNumber = Math.floor(Math.random() * 500) + 1;
+        const randomNumber = Math.floor(Math.random() * 1250) + 1;
         let profileData = await profileModel.findOne({ userID: message.author.id });
         if (!profileData) {
             let profile = await profileModel.create({
@@ -24,10 +24,19 @@ module.exports = {
 
             if (talkedRecently.has(message.author.id)) {
                 const talked = new Discord.MessageEmbed()
-                .setDescription("you need to wait for this command to refresh. - " + message.author.username);
+                    .setDescription("you need to wait for this command to refresh. - " + message.author.username);
                 message.channel.send(talked)
             }
             else {
+                let stolenfrom = message.mentions.users.first()
+                if (!stolenfrom) return message.channel.send("please mention a user")
+                if (stolenfrom) (console.log(stolenfrom.id))
+
+                let person = await profileModel.findOne({ userID: stolenfrom.id });
+
+
+                if (!person) return message.channel.send("That person isn't in my database")
+                if (person.coin === 0) return message.channel.send("this person can't be stolen from as they have **0 Chaos Credits**")
 
                 let convstring = Number(randomNumber)
 
@@ -35,20 +44,13 @@ module.exports = {
                 let failrand = Math.floor((Math.random() * failorsuccess.length))
                 let failss = failorsuccess[failrand]
 
-                //the beg money givers
-                const beggiver = [`Dwayne Johnson`, `Selena Gomez`, `Justin Bieber`, `Rick Astley`, `Alan Walker`, `Random Stranger`,]
-                let begresultgive = Math.floor((Math.random() * beggiver.length))
-                let beggive = beggiver[begresultgive]
-
                 // description when you use beg command
-                const begdescription = [`You little poor beggar. here take **$${convstring} Chaos credits**`, `Here take **$${convstring} Chaos credits** and invest it.`, `Aw man i only got $30 in my wallet.. I can give you **$${convstring} Chaos credits**`]
-                let begresultdesc = Math.floor((Math.random() * begdescription.length))
-                let begres = begdescription[begresultdesc]
+                const begdescription = [`You've successfully stolen **${convstring} Chaos Credits** from That user`]
+                let begres = begdescription
 
                 // description when you failed at begging
-                const beggingfail = ['Get a job man', "kekw here's $200 ||sike||", 'get out of my sight', 'go ask your momma', 'ask someone else', 'go ask somewhere else', 'you failed to recieve money from someone']
-                let failbegrand = Math.floor((Math.random() * beggingfail.length))
-                let failbeg = beggingfail[failbegrand]
+                const beggingfail = ["You've Failed to steal from that user"]
+                let failbeg = beggingfail
 
 
                 if (failss === 'Success') {
@@ -60,8 +62,16 @@ module.exports = {
                             coin: convstring,
                         }
                     })
+
+                    const other = await profileModel.findOneAndUpdate({
+                        userID: message.mentions.users.first().id,
+                    }, {
+                        $inc: {
+                            coin: -convstring,
+                        }
+                    })
+
                     const begembed = new Discord.MessageEmbed()
-                        .setTitle(`${beggive}`)
                         .setDescription(`${begres}`)
                         .setColor("#fcba03")
                         .setTimestamp(Date.now())
@@ -72,7 +82,6 @@ module.exports = {
                 } else if (failss === 'Failed') {
 
                     const failembed = new Discord.MessageEmbed()
-                        .setTitle(`**${beggive}**`)
                         .setDescription(`**${failbeg}`)
                         .setColor("#fcba03")
                         .setTimestamp(Date.now())
--- commands/economy/work.js
+++ commands/economy/work.js
@@ -3,13 +3,13 @@ const Discord = require('discord.js')
 const talkedRecently = new Set();
 
 module.exports = {
-    name: 'beg',
+    name: 'work',
     aliases: [],
     category: 'economy',
     utilisation: '{prefix}help <command name>',
 
     async execute(client, message, args) {
-        const randomNumber = Math.floor(Math.random() * 500) + 1;
+        const randomNumber = Math.floor(Math.random() * 1250) + 1;
         let profileData = await profileModel.findOne({ userID: message.author.id });
         if (!profileData) {
             let profile = await profileModel.create({
@@ -31,27 +31,10 @@ module.exports = {
 
                 let convstring = Number(randomNumber)
 
-                const failorsuccess = ['Success', 'Failed']
-                let failrand = Math.floor((Math.random() * failorsuccess.length))
-                let failss = failorsuccess[failrand]
-
-                //the beg money givers
-                const beggiver = [`Dwayne Johnson`, `Selena Gomez`, `Justin Bieber`, `Rick Astley`, `Alan Walker`, `Random Stranger`,]
-                let begresultgive = Math.floor((Math.random() * beggiver.length))
-                let beggive = beggiver[begresultgive]
-
                 // description when you use beg command
-                const begdescription = [`You little poor beggar. here take **$${convstring} Chaos credits**`, `Here take **$${convstring} Chaos credits** and invest it.`, `Aw man i only got $30 in my wallet.. I can give you **$${convstring} Chaos credits**`]
+                const begdescription = [`You Worked in a Pizza Shop and got paid **${convstring} Chaos credits**`, `You stole someones wallet and got **${convstring} Chaos credits**`, `You worked as a Taxi driver and revieved **${convstring} Chaos credits**`, `You worked a double shift and earned **${convstring} Chaos credits**`, `You worked at a Gas Station and got **${convstring} Chaos credits**`, `You were in the Coal Mines and made **${convstring} Chaos credits**`, `You  found **${convstring} Chaos credits** in your car`]
                 let begresultdesc = Math.floor((Math.random() * begdescription.length))
                 let begres = begdescription[begresultdesc]
-
-                // description when you failed at begging
-                const beggingfail = ['Get a job man', "kekw here's $200 ||sike||", 'get out of my sight', 'go ask your momma', 'ask someone else', 'go ask somewhere else', 'you failed to recieve money from someone']
-                let failbegrand = Math.floor((Math.random() * beggingfail.length))
-                let failbeg = beggingfail[failbegrand]
-
-
-                if (failss === 'Success') {
 
                     const response = await profileModel.findOneAndUpdate({
                         userID: message.author.id,
@@ -61,7 +44,6 @@ module.exports = {
                         }
                     })
                     const begembed = new Discord.MessageEmbed()
-                        .setTitle(`${beggive}`)
                         .setDescription(`${begres}`)
                         .setColor("#fcba03")
                         .setTimestamp(Date.now())
@@ -69,17 +51,7 @@ module.exports = {
                     //send the begembed in channel
                     message.channel.send(begembed)
 
-                } else if (failss === 'Failed') {
-
-                    const failembed = new Discord.MessageEmbed()
-                        .setTitle(`**${beggive}**`)
-                        .setDescription(`**${failbeg}`)
-                        .setColor("#fcba03")
-                        .setTimestamp(Date.now())
-
-                    //send the failembed in channel
-                    message.channel.send(failembed)
-                }
+                
                 talkedRecently.add(message.author.id);
                 setTimeout(() => {
                     // Removes the user from the set after a minute
