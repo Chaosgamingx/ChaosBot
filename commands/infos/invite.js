48d99db6a97185410760655e98aa7bd8374f4d20
Chaosgamingx <78837643+Chaosgamingx@users.noreply.github.com>
2021-09-16 17:52:46 -0500

    Add files via upload

--- commands/infos/invite.js
+++ commands/infos/invite.js
@@ -1,4 +1,4 @@
-const buttons = require("discord-buttons")
+
 const Discord = require('discord.js')
 module.exports = {
   name: "invite",
@@ -8,16 +8,10 @@ module.exports = {
 
   async execute(client, message, args) {
 
-
     const embed = new Discord.MessageEmbed()
-      .setDescription("Hello there, if you want to invite ChaosBot to your server then just click on that little box below this embed, hope you enjoy")
-
-
-    let button = new buttons.MessageButton()
-      .setStyle('url')
-      .setURL('https://npmjs.com/discord-buttons')
-      .setLabel("ChaosBot's invite link");
-
-    message.channel.send(embed, button);
-  }
-};
+		.setColor("RANDOM")
+		.addField("IF you need help with chaosvot then feel free to ask in the official ChaosBot server [here](https://discord.gg/PRzWKj4dBy)")
+		.addField("If you want to add ChaosBot to your server", "than click [here](https://discord.com/oauth2/authorize?client_id=530267263501074443&scope=bot&permissions=2147483647)")
+		.setTimestamp()
+	message.channel.send(embed)
+}}
