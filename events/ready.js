4b74c5e9d0be670ff9c8b18e2a4cfc4079336929
Chaosgamingx <78837643+Chaosgamingx@users.noreply.github.com>
2021-10-02 11:53:00 -0500

    Delete events directory

--- events/ready.js
+++
@@ -1,5 +0,0 @@
-module.exports = async (client) => {
-    console.log(`Logged in as ${client.user.username}. Ready on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users`);
-
-    client.user.setActivity(client.config.discord.activity);
-};
