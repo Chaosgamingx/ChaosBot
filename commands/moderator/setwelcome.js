08ffe2f8570d833cdc0a3a72a5938b02a99e72ee
Chaosgamingx <78837643+Chaosgamingx@users.noreply.github.com>
2021-10-12 09:25:27 -0500

    new custom welcome message

---
+++ commands/moderator/setwelcome.js
@@ -0,0 +1,35 @@
+const welcomeSchema = require("../../models/welcomeSchema")
+const Discord = require("discord.js")
+
+module.exports = {
+    name: 'setwelcome',
+    aliases: ['welcome'],
+    category: 'Core',
+    utilisation: '{prefix}setwelcome (channel)',
+
+    execute(client, message, args) {
+        if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("you are not a moderator")
+    
+        const channel = message.mentions.channels.first()
+        if(!channel) return message.channel.send("please specify a channel")
+
+        const msg = args.slice(1).join(" ")
+        if(!msg) return message.channel.send("What do you want me to say when someone joins")
+
+
+        welcomeSchema.findOne({ guildId: message.guild.id }, async (err, data) => {
+            if(data) {
+                data.channelId = channel.id,
+                data.welcomeMSG = msg
+                data.save()
+            } else {
+                new welcomeSchema({
+                    guildId: message.guild.id,
+                    welcomeMSG: msg,
+                    channelId: channel.id,
+                }).save()
+            }
+            message.channel.send(`the welcome channel has been set to ${channel}`)
+        })
+    }
+}
