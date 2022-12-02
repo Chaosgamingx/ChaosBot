52dbde365b1cf00b90863b22b3f9adbbd89fe25d
Chaosgamingx <lcnewc5558@gmail.com>
2022-12-01 23:32:00 -0600

    Add files via upload

--- data/models/profileSchema.js
+++ data/models/profileSchema.js
@@ -6,6 +6,12 @@ let profileSchema = new mongoose.Schema({
     coin: { type: Number, default: 1000 },
     bank: { type: Number },
     items: { type: Array, require: false, default: [] },
+    level: { type: Number },
+    xp: { type: Number },
+    class: { type: String, require: true },
+    weaponid: { type: String, require: true },
+    armorid: { type: String, require: true },
+    accessoryid: { type: String, require: true },
 })
 
 const model = mongoose.model('profileModels', profileSchema);
