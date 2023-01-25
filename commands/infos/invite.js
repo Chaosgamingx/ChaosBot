43e78883e0fe44b83fc5c81a4549b5ae9aaf9e65
Chaosgamingx <lcnewc5558@gmail.com>
2023-01-25 16:05:50 -0600

    Fixed invite link

--- commands/infos/invite.js
+++ commands/infos/invite.js
@@ -13,7 +13,7 @@ module.exports = {
     const embed = new Discord.MessageEmbed()
 		.setColor("RANDOM")
 		.addField("IF you need help with chaosBot then feel free to ask in the official ChaosBot server", "[here](https://discord.gg/cSJuaWBBc7)")
-		.addField("If you want to add ChaosBot to your server", "than click [here](https://discord.com/oauth2/authorize?client_id=530267263501074443&permissions=1644971949559&scope=bot%20applications.commands)")
+		.addField("If you want to add ChaosBot to your server", "than click [here](https://discord.com/oauth2/authorize?client_id=1023731050251296769&permissions=4374424190327&scope=applications.commands%20bot)")
 		.setTimestamp()
 	message.channel.send(embed)
 }}
